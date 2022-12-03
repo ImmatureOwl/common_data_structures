47b4624894a359cc644935bf9804652c23cc3e46
Matteo Poli <98114773+ImmatureOwl@users.noreply.github.com>
2022-12-03 12:13:52 +0100

    Updated Dictionary.c

--- Dictionary.c
+++ Dictionary.c
@@ -10,6 +10,7 @@
 
 SetNode *new_node(SetNode *node, cString *key, size_t key_len);
 StringItem *string_item_new(cString *key, cString *string, size_t key_len);
+//SetNode *get_index_head(SetTable *table, cString *key, const size_t key_len);
 
 int set_collisions = 0;
 
@@ -28,6 +29,7 @@ struct set_node
     SetNode *next;
     cString *key;
     size_t key_len;
+    size_t value;
 };
 
 struct set_string_item
@@ -35,21 +37,21 @@ struct set_string_item
     SetNode *node;
     cString *key;
     size_t key_len;
-    cString *string;
+    cString *value;
 };
 
-StringItem *string_item_new(cString *key, cString *string, size_t key_len)
+StringItem *string_item_new(cString *key, cString *value, size_t key_len)
 {
-    printf("0_a\n");
+    //printf("0_a\n");
     StringItem *item = malloc(sizeof(StringItem));
     if (!item) return NULL;
-    printf("1_a\n");
+    //printf("1_a\n");
     item->node = malloc(sizeof(SetNode));
     item->node->key = key;
     item->node->key_len = key_len;
     item->key = key;
     item->key_len = key_len;
-    item->string = string;
+    item->value = value;
     return item;
 }
 
@@ -63,10 +65,10 @@ struct set_int_item
 
 IntItem *int_item_new(cString *key, int value, size_t key_len)
 {   
-    printf("0_b\n");
+    //printf("0_b\n");
     IntItem *item = malloc(sizeof(IntItem));
     if(!item) return NULL;
-    printf("1_b\n");
+    //printf("1_b\n");
     item->node = malloc(sizeof(SetNode));
     item->node->key = key;
     item->node->key_len = key_len;
@@ -99,31 +101,39 @@ SetTable *set_table_new (const size_t hashmap_size )
 SetNode *set_insert(SetTable *table, SetNode *node)
 {   
     printf("storing key: %s\n", node->key);
-    printf("2\n");
+    //printf("2\n");
     size_t hash = djb33x_hash(node->key, node->key_len);
     size_t index = hash % table->hashmap_size;
     printf("stored index is: %llu\n", index);
     SetNode *head = table->nodes[index];
     if (!head)
     {
-        printf("3_a\n");
+        //printf("3_a\n");
         table->nodes[index] = malloc(sizeof(SetNode));
         if (!table->nodes[index]) return NULL;
-        printf("4_a\n\n");
+        //printf("4_a\n\n");
         table->nodes[index]->key = node->key;
         table->nodes[index]->key_len = node->key_len;
+        table->nodes[index]->value = node->value;
         table->nodes[index]->next = NULL;
         return table->nodes[index];
     }
-    printf("3_b\n");
+    //printf("3_b\n");
     set_collisions++;
     SetNode *new_item = malloc(sizeof(SetNode));
     if (!new_item) return NULL;
-    printf("4_b\n\n");
+    //printf("3_bb\n");
     new_item->key = node->key;
     new_item->key_len = node->key_len;
+    new_item->value = node->value;
     new_item->next = NULL;
     SetNode *tail = head;
+    if (!head->next)
+    {
+        head->next = new_item;
+        //printf("4_b\n\n");
+        return new_item;
+    }
     while (head)
     {
         tail = head;
@@ -131,6 +141,7 @@ SetNode *set_insert(SetTable *table, SetNode *node)
         if (head->key == node->key) return NULL; //don't add duplicates to the table
     }
     tail->next = new_item;
+    //printf("4_b\n\n");
     return new_item;
 }
 
@@ -138,6 +149,7 @@ SetNode *get_index_head(SetTable *table, cString *key, const size_t key_len)
 {
     size_t hash = djb33x_hash(key, key_len);
     size_t index = hash % table->hashmap_size;
+    printf("found head at index: %llu\n", index);
     return table->nodes[index];
 }
 
@@ -165,6 +177,7 @@ SetNode *set_search_key(SetTable *table, cString *key, const size_t key_len)
             return NULL; 
         }//end of cycle
     }
+    printf("found key: %s\n, value: %llu", head->key, head->value);
     return head;
 }
 
@@ -210,9 +223,13 @@ int main()
     
     set_insert(table, (SetNode *)int_item_new("key5", 45, hashmap_size));
     set_insert(table, (SetNode *)int_item_new("key6", 234, hashmap_size));
-    
 
-    StringItem *string_item = (StringItem *)set_search_key(table, "key1", hashmap_size);
-    IntItem *int_item = (IntItem *)set_search_key(table, "key5", hashmap_size);
+
+    StringItem *string_item = malloc(sizeof(StringItem));
+    IntItem *int_item = malloc(sizeof(IntItem));
+    string_item = (StringItem *)set_search_key(table, "key1", hashmap_size);
+    int_item = (IntItem *)set_search_key(table, "key5", hashmap_size);
+    printf("string_item->key: %s, string_item->value: %s\n", string_item->key, string_item->value);
+    printf("int_item->key: %s, int_item->value: %d\n", int_item->key, int_item->value);
     return 0;
 }
